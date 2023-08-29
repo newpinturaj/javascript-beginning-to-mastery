2a78e04af43e98caec8fc0b7ce027e402b183fa1
PINTU RAJ <newpinturaj@gmail.com>
2023-08-30 01:37:33 +0530

    edited 27.js file

--- part1/27.js
+++ part1/27.js
@@ -1,12 +1,18 @@
 // array push pop 
+// push --> use to insert at the end, also return the length after insertion
+// pop --> use to remove from last, also return the removed item
 
 // array shift unshift 
+// unshift --> use to add at the begining, also return lenght after insertion
+// shift --> use to remove from begining, also return the removed item
+
 
 let fruits = ["apple", "mango", "grapes"];
 console.log(fruits);
-// push 
+// push
 // fruits.push("banana");
 // console.log(fruits);
+
 // pop 
 // let poppedFruit = fruits.pop();
 // console.log(fruits);
@@ -17,7 +23,7 @@ console.log(fruits);
 // fruits.unshift("myfruit");
 // console.log(fruits);
 
-// shift 
+// shift
 // let removedFruit = fruits.shift();
 // console.log(fruits);
 // console.log("removed fruits is ", removedFruit);
